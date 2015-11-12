4a399b896640cff3a1d6b362e31cd4034c2048dc
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-11-11 21:51:12 -0800

    Pagination, search filter working.

--- public/concerts.js
+++ public/concerts.js
@@ -34,11 +34,34 @@ angular.module('beatssounds.concerts', ['ui.bootstrap'])
       auth.getConcerts().then(function(resp) {
         $scope.isLoading = false;
         $scope.data = resp;
+        $scope.paginate();
+        // $scope.pageCounter();
       })
     };
 
     $scope.getConcerts()
 
     /// PAGINATION ==========================================
-    
+    $scope.itemsPerPage = 4
+    $scope.currentPage = 1;
+
+    $scope.pageCount = function () {
+      return Math.ceil($scope.data.length / $scope.itemsPerPage);
+    };
+
+    $scope.paginate = function () {
+      $scope.totalItems = $scope.data.length;
+      $scope.$watch('currentPage + itemsPerPage', function() {
+        var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+        var end = begin + $scope.itemsPerPage;
+        $scope.filteredEvents = $scope.data.slice(begin, end);
+      
+        var counter = $scope.data.length / $scope.itemsPerPage;
+        if ($scope.currentPage <= counter) {
+          $scope.showing = $scope.itemsPerPage;
+        } else {
+          $scope.showing = $scope.data.length;
+        }
+      });
+    };
   });
