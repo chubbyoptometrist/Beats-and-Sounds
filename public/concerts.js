655355572e2cf88d748f91eec540580361033868
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-11-11 20:45:20 -0800

    Added styles

--- public/concerts.js
+++ public/concerts.js
@@ -32,32 +32,13 @@ angular.module('beatssounds.concerts', ['ui.bootstrap'])
     $scope.getConcerts = function() {
       this.isLoading = true;
       auth.getConcerts().then(function(resp) {
-        console.log(resp);
         $scope.isLoading = false;
         $scope.data = resp;
-        $scope.eventsCounter();
       })
     };
 
     $scope.getConcerts()
+
     /// PAGINATION ==========================================
-    $scope.filteredEvents = [];
-    $scope.currentPage = 1;
-    $scope.numPerPage = 6;
-    $scope.maxSize = 6;
-
-    $scope.$watch("currentPage + numPerPage", function () {
-      var begin = (($scope.currentPage - 1) * $scope.numPerPage);
-      var end = begin + $scope.numPerPage;
-
-      $scope.filteredEvents = $scope.data.slice(begin, end);
-    });
-    $scope.eventsCounter = function () {
-      var num = $scope.data.length / 6;
-      if ($scope.currentPage <= num) {
-        $scope.current = 6;
-      } else {
-        $scope.current = $scope.data.length-(6*num);
-      }
-    };
+    
   });
