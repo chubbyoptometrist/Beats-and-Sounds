1a076455944631ab36d79cad3fc96c623a25a10d
Abel <ablwang@ucdavis.edu>
2015-11-16 15:07:33 -0800

    messing with mySQL settimeout

--- server/db/connect.js
+++ server/db/connect.js
@@ -18,12 +18,11 @@ var connection;
 
 function handleDisconnect() {
   connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
+                                                  // the old one cannot be reused.                                                  
   connection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 300); // We introduce a delay before attempting to reconnect,
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
@@ -38,5 +37,5 @@ function handleDisconnect() {
 }
 
 handleDisconnect();
-
+setInterval(function() {connection.query('SELECT 1');}, 5000); 
 module.exports = connection;
