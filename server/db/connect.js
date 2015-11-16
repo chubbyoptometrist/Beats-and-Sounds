1d9683c0025ba9ff809be8c3f23dcf0dbe0f217c
Abel <ablwang@ucdavis.edu>
2015-11-16 04:59:03 -0800

    Modify mySQL config to handle errors

--- server/db/connect.js
+++ server/db/connect.js
@@ -7,14 +7,36 @@ var password = process.env.DB_PASSWORD || 'laura';
 var database = process.env.DB_DATABASE || 'chubbySongDB'; 
 
 
-var dbConnection = mysql.createConnection({
+var db_config = {
   host: host,
   user: user,
   password: password,
   database: database
-});
+};
 
-// Connecting to db, see connect.js for details
-dbConnection.connect();
+var connection;
 
-module.exports = dbConnection;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
