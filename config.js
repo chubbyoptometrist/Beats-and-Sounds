578c92498bd485d0e1067fa22373219787ea46d7
Abel <ablwang@ucdavis.edu>
2015-11-16 16:06:14 -0800

    test

---
+++ config.js
@@ -0,0 +1,6 @@
+//SPOTIFY STUFF
+module.exports.client_id = 'd4a783beb6d6481083d8398c342b38ba';
+module.exports.client_secret = 'af9caca1fc80432da09939a59d4faa6f';
+
+//SONGKICK STUFF
+module.exports.api_key = 'OvaAdrKQm2jFurxi';
